c9852fdbb1359cb71cc3ddb535d537f2307ca661
Paris <203000868+pdunmire-pcd@users.noreply.github.com>
2026-04-21 17:39:04 -0700

    added the sitemap.js file

---
+++ app/sitemap.js
@@ -0,0 +1,40 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://thepreparedbirth.com',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: 'https://thepreparedbirth.com/about',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://thepreparedbirth.com/faq',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://thepreparedbirth.com/inquiry',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://thepreparedbirth.com/working-with-me',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://thepreparedbirth.com/private-pricing',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+  ];
+}
